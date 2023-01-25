7a645c358fcc113893fda09a2675b398255ff2f2
Crowdbotics <team@crowdbotics.com>
2023-01-25 00:30:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bluetube_38446/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bluetube_38446;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "bluetube_38446";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bluetube-38446",
+  url: "https://bluetube-38446.botics.co",
+  api: "https://bluetube-38446.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
